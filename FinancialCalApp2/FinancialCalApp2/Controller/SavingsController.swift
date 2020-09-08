6bdd2d072e639eef1ddd8bad3facbf81156cabad
MahelM <35723398+DevXtreme0@users.noreply.github.com>
2020-09-08 15:47:47 +0530

    Update SavingsController.swift

--- FinancialCalApp2/FinancialCalApp2/Controller/SavingsController.swift
+++ FinancialCalApp2/FinancialCalApp2/Controller/SavingsController.swift
@@ -2,8 +2,8 @@
 //  SavingsController.swift
 //  FinancialCalApp2
 //
-//  Created by Mahel Manjitha Mawellage on 3/5/20.
-//  Copyright © 2020 Mahel Manjitha Mawellage. All rights reserved.
+//  Created by MahelM on 3/5/20.
+//  Copyright © 2020 MahelM. All rights reserved.
 //
 
 import Foundation
